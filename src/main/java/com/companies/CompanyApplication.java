bc99561c481c7c5577d18f1e6cec6790930b1ed0
john <jhancock1975@gmail.com>
2016-01-31 09:48:57 -0500

    add code so application can start in tomcat

--- src/main/java/com/companies/CompanyApplication.java
+++ src/main/java/com/companies/CompanyApplication.java
@@ -2,16 +2,23 @@ package com.companies;
  
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.context.web.SpringBootServletInitializer;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+
  
 @Configuration
 @ComponentScan
 @EnableAutoConfiguration
-public class CompanyApplication {
+public class CompanyApplication extends SpringBootServletInitializer{
  
     public static void main(String[] args) {
         SpringApplication.run(CompanyApplication.class, args);
     }
+    
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(CompanyApplication.class);
+     }
  
 }
